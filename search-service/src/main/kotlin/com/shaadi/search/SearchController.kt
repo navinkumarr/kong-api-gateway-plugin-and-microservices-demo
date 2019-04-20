092dbcaa36e559d8c8437c7fe92629837f1a7019
Navinkumar S <navinkumar.s@peopleinteractive.in>
2019-04-20 17:35:26 +0530

    caching search

--- search-service/src/main/kotlin/com/shaadi/search/SearchController.kt
+++ search-service/src/main/kotlin/com/shaadi/search/SearchController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*
 
 import com.github.kittinunf.fuel.*;
 import com.github.kittinunf.fuel.jackson.*;
+import com.fasterxml.jackson.module.kotlin.*
 
 enum class Gender{ Male, Female}
 enum class ProfileStatus{ Active, Deactivated}
@@ -14,6 +15,8 @@ enum class ProfileStatus{ Active, Deactivated}
 data class Profile(val basic: Basic, val account: Account)
 data class Basic (val age: Int, val gender: Gender , val first_name: String, val last_name: String)
 data class Account (val status: ProfileStatus, val memberlogin: String)
+
+data class Cache (val key: String, val value: String, val id: Int?, val expiry: Long)
 
 typealias Profiles = List<Profile>
 
@@ -24,19 +27,45 @@ class SearchController {
 
     @GetMapping("/search")
     fun profiles(@RequestParam(value = "profileids") profileids: String) : SearchResponse {
-        return fetchProfiles(profileids)
-    }
 
-    fun fetchFromCache(profileids: String): searchResponse{
-        val (request, response, result) = Fuel.get("http://localhost:8081/cache/${profileids}")
-            .responseObject<SearchResponse>()
-
-        // not handling errors
-        var (searchResponse, error) = result;
+        var searchResponse = fetchFromCache(profileids)
 
         if(searchResponse == null){
-            searchResponse = SearchResponse(data = listOf())
+            searchResponse = fetchProfiles(profileids)
+            if(searchResponse.data.size > 0) {
+                saveInCache(profileids, searchResponse)
+            }
         }
+
+        return searchResponse
+    }
+
+    fun saveInCache(profileids: String, searchResponse: SearchResponse) {
+        val mapper = jacksonObjectMapper()
+        val unixTime = System.currentTimeMillis() / 1000;
+
+        val cacheData = Cache(id = null, key = profileids, value = mapper.writeValueAsString(searchResponse.data), expiry = unixTime + 60)
+
+        Fuel.post("http://localhost:8080/cache/")
+            .body(mapper.writeValueAsString(cacheData))
+    }
+
+    fun fetchFromCache(profileids: String): SearchResponse? {
+        val (request, response, result) = Fuel.get("http://localhost:8080/cache/${profileids}")
+            .responseObject<ArrayList<Cache>>()
+
+        // not handling errors
+        var (cacheResponse, error) = result;
+
+        if(cacheResponse == null  || cacheResponse.size == 0){
+            return null
+        }
+
+        val mapper = jacksonObjectMapper()
+
+        val profiles = mapper.readValue<Profiles>(cacheResponse.get(0).value)
+
+        val searchResponse = SearchResponse(data = profiles)
 
         return searchResponse
     }
